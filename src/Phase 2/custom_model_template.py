8db30cbe55d290ae888e95e4e387ba12c8a7462a
Arda <arda.bakici04@gmail.com>
2021-08-02 01:07:01 +0300

    Added some code

---
+++ src/Phase 2/custom_model_template.py
@@ -0,0 +1,13 @@
+# This file provides a template for creating custom models for segmantation models framework. This
+# file shouldn't be overridden instead it should be copied and used
+from tensorflow import keras
+
+def build_model() -> keras.Model:
+    """ This function is used for building your model. Function must return a keras.Model type. Input of the model
+        must be the input of the backbone and all the function layers must be put on top of the backbone output """
+    pass
+
+def model() -> keras.Model:
+    """ This function is the main called function and using the helper functions defined above you must create
+        a keras model and return it """
+    pass
